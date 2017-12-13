05f585c4882d6ccc5551e1f231d708f928c006ba
Danila Tsvetkov <dtsvetkov@itdhq.com>
2017-12-13 09:46:43 +0300

    Refactor, get fields for export

--- src/main/java/com/alvexcore/repo/datalist/RegistryAsXLSX.java
+++ src/main/java/com/alvexcore/repo/datalist/RegistryAsXLSX.java
@@ -3,6 +3,8 @@ package com.alvexcore.repo.datalist;
 import com.alvexcore.repo.foldersize.FolderSizeService;
 import org.alfresco.model.ContentModel;
 import org.alfresco.repo.content.MimetypeMap;
+import org.alfresco.service.cmr.dictionary.DictionaryService;
+import org.alfresco.service.cmr.dictionary.PropertyDefinition;
 import org.alfresco.service.cmr.repository.ChildAssociationRef;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.cmr.repository.NodeService;
@@ -15,6 +17,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.extensions.webscripts.*;
+
 import java.io.*;
 import java.util.*;
 
@@ -36,6 +39,12 @@ public class RegistryAsXLSX extends AbstractWebScript {
 
     public void setNodeService(NodeService nodeService) {
         this.nodeService = nodeService;
+    }
+
+    private DictionaryService dictionaryService;
+
+    public void setDictionaryService(DictionaryService dictionaryService) {
+        this.dictionaryService = dictionaryService;
     }
 
     private SiteService siteService;
@@ -78,17 +87,16 @@ public class RegistryAsXLSX extends AbstractWebScript {
                 exclude = jsonObj.getJSONArray("exclude");
             }
 
-        if (dataListNode==null) {
-            for(int i = 0; i < nodesList.length(); siteNodes.add((String) nodesList.get(i++)));
-        }
+            if (dataListNode==null) {
+                for(int i = 0; i < nodesList.length(); siteNodes.add((String) nodesList.get(i++)));
+            }
 
-        if (include!=null) {
-//            System.out.println(include.get(0));
-            for (int i = 0; i < include.length(); includeList.add((String) include.get(i++)));
-        }
-        else if (exclude!=null) {
-            for (int i = 0; i < exclude.length(); excludeList.add((String) exclude.get(i++)));
-        }
+            if (include!=null) {
+                for (int i = 0; i < include.length(); includeList.add((String) include.get(i++)));
+            }
+            else if (exclude!=null) {
+                for (int i = 0; i < exclude.length(); excludeList.add((String) exclude.get(i++)));
+            }
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -97,23 +105,38 @@ public class RegistryAsXLSX extends AbstractWebScript {
         try {
 
             Workbook wb = null;
-            for (String property: includeList){
-                if (!namespaceService.getPrefixes().contains(property.split(":")[0])){
-                    webScriptResponse.setStatus(Status.STATUS_BAD_REQUEST);
-//            webScriptResponse.getWriter().write();
-                    throw new WebScriptException(Status.STATUS_BAD_REQUEST, "ContentModel doesn't contain property: " + property);
 
+            for (String property: includeList){
+//                System.out.println(dictionaryService.getProperty(QName.createQName(property)));
+
+//                System.out.println(QName.resolveToQName(namespaceService, property));
+//                dictionaryService.getType(nodeService.getType(itemsNodes.get(0)));
+//                System.out.println("-" + dictionaryService.getProperty(QName.resolveToQName(namespaceService, property)));
+//                System.out.println(null);
+//                if (dictionaryService.getProperty(QName.resolveToQName(namespaceService, property))==null){
+                if (QName.resolveToQName(namespaceService, property)==null){
+                    webScriptResponse.setStatus(Status.STATUS_BAD_REQUEST);
+//                  webScriptResponse.getWriter().write();
+                    throw new WebScriptException(Status.STATUS_BAD_REQUEST, "ContentModel doesn't contain property: " + property);
                 }
-//                System.out.println(property.split(":")[0]);
-//                System.out.println(namespaceService.getPrefixes());
             }
 
             if (dataListNode==null) {
-                wb = createXlsxRegisters(XLS_SHEET_NAME, siteNodes, includeList, excludeList);
+                List<NodeRef> itemsNodes = new ArrayList<>();
+                for (int k=0; k<siteNodes.toArray().length; itemsNodes.add(new NodeRef(siteNodes.get(k++))));
+
+                wb = createWorkbook(XLS_SHEET_NAME, includeList, excludeList, itemsNodes);
+//                wb = createXlsxRegisters(XLS_SHEET_NAME, siteNodes, includeList, excludeList);
             }
             else {
+                List<ChildAssociationRef> itemsNodesTest = nodeService.getChildAssocs(new NodeRef(dataListNode), ContentModel.ASSOC_CONTAINS, RegexQNamePattern.MATCH_ALL);
+                List<NodeRef> itemsNodes = new ArrayList<>();
+                for (int k=0; k<itemsNodesTest.toArray().length; itemsNodes.add(itemsNodesTest.get(k++).getChildRef()));
+
+                wb = createWorkbook(XLS_SHEET_NAME, includeList, excludeList, itemsNodes);
+
 //                System.out.println(nodeService.getChildAssocs(new NodeRef(dataListNode), ContentModel.ASSOC_CONTAINS, RegexQNamePattern.MATCH_ALL).size());
-                wb = createXlsxRegisters(XLS_SHEET_NAME, dataListNode, includeList, excludeList);
+//                wb = createXlsxRegisters(XLS_SHEET_NAME, dataListNode, includeList, excludeList);
             }
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             wb.write(baos);
@@ -121,189 +144,127 @@ public class RegistryAsXLSX extends AbstractWebScript {
             webScriptResponse.setContentEncoding("UTF-8");
             webScriptResponse.setContentType(MimetypeMap.MIMETYPE_EXCEL);
             webScriptResponse.getOutputStream().write(baos.toByteArray());
-
-        } catch (NullPointerException e) {
-
+//            MimetypeMap.
+        } catch (WebScriptException e) {
+            webScriptResponse.setStatus(Status.STATUS_BAD_REQUEST);
+            throw new WebScriptException(Status.STATUS_BAD_REQUEST, e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
 //            webScriptResponse.setStatus(400);
 //            System.out.println("Didn't work " + e);
 //            System.out.println(Status.STATUS_BAD_REQUEST);
 //            webScriptResponse.setStatus(Status.STATUS_BAD_REQUEST);
 //            webScriptResponse.getWriter().write();
-//            throw new WebScriptException(Status.STATUS_BAD_REQUEST, "Something went horribly wrong");
+//            throw new WebScriptException("Something went horribly wrong.");
 
         }
     }
 
 
-    protected Workbook createXlsxRegisters(String XLS_SHEET_NAME, String dataListNode, List<String> includeList, List<String> excludeList) {
+    protected Workbook createWorkbook (String XLS_SHEET_NAME, List<String> includeList, List<String> excludeList, List<NodeRef> itemsNodes ){
 
-        try {
-            Workbook wb = new XSSFWorkbook();
-            CreationHelper createHelper = wb.getCreationHelper();
-            Sheet sheet = wb.createSheet(XLS_SHEET_NAME);
+        Workbook wb = new XSSFWorkbook();
 
-            int rowNum = 0;
-            int cellNum;
+        CreationHelper createHelper = wb.getCreationHelper();
+        Sheet sheet = wb.createSheet(XLS_SHEET_NAME);
 
-            List<ChildAssociationRef> itemsNodes = nodeService.getChildAssocs(new NodeRef(dataListNode), ContentModel.ASSOC_CONTAINS, RegexQNamePattern.MATCH_ALL);
+        int rowNum = 0;
 
-            if (includeList.toArray().length>0) {
-//                Map<QName, Serializable> variable = nodeService.getProperties(itemsNodes.get(0).getChildRef());
-                Map<QName, Serializable> map = new HashMap<QName, Serializable>();
-                for (String str: includeList){
-                    if (nodeService.getProperty(itemsNodes.get(0).getChildRef(), QName.resolveToQName(namespaceService, str))!=null)
-                    map.put(QName.resolveToQName(namespaceService, str) , nodeService.getProperty(itemsNodes.get(0).getChildRef(), QName.createQName(str, namespaceService)));
-                }
-                cellNum = 0;
-                Row row = sheet.createRow((short) rowNum);
-                for (QName i : map.keySet()) {
-                        row.createCell(cellNum).setCellValue(createHelper.createRichTextString(i.toPrefixString(namespaceService)));
-                        cellNum++;
-                }
-                rowNum++;
+        if (includeList.toArray().length>0) {
+            Map<QName, Serializable> map = new HashMap<QName, Serializable>();
+            List<QName> headers = new ArrayList<>();
+            List<String> values = new ArrayList<>();
 
-                for (ChildAssociationRef item : itemsNodes) {
-
-                    map.clear();
-                    for (String str: includeList){
-                        if (nodeService.getProperty(item.getChildRef(), QName.resolveToQName(namespaceService, str))!=null)
-                            map.put(QName.resolveToQName(namespaceService, str) , nodeService.getProperty(item.getChildRef(), QName.resolveToQName(namespaceService, str)));
-                    }
-
-                    cellNum = 0;
-                    row = sheet.createRow((short) rowNum);
-                    for (QName i : map.keySet()) {
-                        row.createCell(cellNum).setCellValue(createHelper.createRichTextString(map.get(i).toString()));
-                        cellNum++;
-                    }
-                    rowNum++;
-
-                }
+            for (String str: includeList){
+//                dictionaryService.getProperty(QName.resolveToQName(namespaceService, str))
+//                System.out.println(QName.resolveToQName(namespaceService, str));
+//                if (nodeService.getProperty(itemsNodes.get(0), QName.resolveToQName(namespaceService, str))!=null) {
+//                if (QName.resolveToQName(namespaceService, str)!=null) {
+                 if (dictionaryService.getProperty(QName.resolveToQName(namespaceService, str))!=null) {
+//                    headers.add(dictionaryService.getProperty(QName.resolveToQName(namespaceService, str)).getName());
+                     headers.add(QName.resolveToQName(namespaceService, str));
+                 }
             }
-            else {
-                Map<QName, Serializable> map = nodeService.getProperties(itemsNodes.get(0).getChildRef());
-                for (String str: excludeList){
-//                    if (nodeService.getProperty(itemsNodes.get(0).getChildRef(), QName.resolveToQName(namespaceService, str))!=null)
-                    map.remove(QName.resolveToQName(namespaceService, str));
+//            System.out.println(dictionaryService.getProperty((QName) map.keySet().toArray()[0]));
+
+            Row row = fillRowHeader(rowNum, headers, createHelper, sheet);
+            rowNum++;
+
+            for (NodeRef item : itemsNodes) {
+                map.clear();
+                values.clear();
+//                for (String str: includeList){
+                for (QName str: headers){
+//                    System.out.println(nodeService.getProperty(item, str));
+
+//                    if (nodeService.getProperty(item, QName.resolveToQName(namespaceService, str))!=null)
+//                    map.put(str, nodeService.getProperty(item, str));
+                    values.add(Objects.toString(nodeService.getProperty(item, str), ""));
                 }
-                cellNum = 0;
-                Row row = sheet.createRow((short) rowNum);
-                for (QName i : map.keySet()) {
-                    row.createCell(cellNum).setCellValue(createHelper.createRichTextString(i.toPrefixString(namespaceService)));
-                    cellNum++;
-                }
+
+                row = fillRowData(rowNum, values, createHelper, sheet);
                 rowNum++;
-
-                for (ChildAssociationRef item : itemsNodes) {
-
-//                    map.clear();
-                    map = nodeService.getProperties(item.getChildRef());
-
-                    for (String str: excludeList){
-//                        if (nodeService.getProperty(item.getChildRef(), QName.resolveToQName(namespaceService, str))!=null)
-                        map.remove(QName.resolveToQName(namespaceService, str));
-                    }
-
-                    cellNum = 0;
-                    row = sheet.createRow((short) rowNum);
-                    for (QName i : map.keySet()) {
-                        row.createCell(cellNum).setCellValue(createHelper.createRichTextString(map.get(i).toString()));
-                        cellNum++;
-                    }
-                    rowNum++;
-
-                }
             }
-            return wb;
-
-        } catch (Exception e) {
-            //
         }
-        return null;
+        else {
+            QName type1 = nodeService.getType(itemsNodes.get(0));
+//            dictionaryService.getType(type1).getProperties().
+//            nodeService.getProperty();
+//            dictionaryService.getType(nodeService.getType(itemsNodes.get(0)));
+            Map<QName, PropertyDefinition> map = dictionaryService.getType(type1).getProperties();
+//            Map<QName, Serializable> map = nodeService.getProperties(itemsNodes.get(0));
+            List<QName> headers = new ArrayList<>();
+            List<String> values = new ArrayList<>();
+
+            headers.addAll(map.keySet());
+
+            //            headers = (List<QName>) map.keySet();
+//            System.out.println(headers);
+
+            for (String str : excludeList) {
+//                System.out.println("+" + dictionaryService.getProperties(ContentModel.PROP_NAME));
+//                headers.add(QName.resolveToQName(namespaceService, str));
+                headers.remove(QName.resolveToQName(namespaceService, str));
+            }
+
+            Row row = fillRowHeader(rowNum, headers, createHelper, sheet);
+            rowNum++;
+
+            for (NodeRef item : itemsNodes) {
+                values.clear();
+//                map = nodeService.getProperties(item);
+                for (QName str : headers) {
+//                    map.remove(QName.resolveToQName(namespaceService, str));
+//                    System.out.println(nodeService.getProperty(item, str));
+                    values.add(Objects.toString(nodeService.getProperty(item, str), ""));
+                }
+
+                row = fillRowData(rowNum, values, createHelper, sheet);
+                rowNum++;
+            }
+        }
+
+        return wb;
     }
 
 
-    protected Workbook createXlsxRegisters(String XLS_SHEET_NAME, List<String> siteNodes, List<String> includeList, List<String> excludeList) {
-
-        try {
-
-            Workbook wb = new XSSFWorkbook();
-            CreationHelper createHelper = wb.getCreationHelper();
-            Sheet sheet = wb.createSheet(XLS_SHEET_NAME);
-
-            int rowNum = 0;
-            int cellNum = 0;
-
-            Row row = sheet.createRow((short) rowNum);
-//            System.out.println(namespaceService.getPrefixes());
-            if (includeList.toArray().length>0) {
-                Map<QName, Serializable> map = new HashMap<QName, Serializable>();
-                for (String str: includeList){
-                    if (nodeService.getProperty(new NodeRef(siteNodes.get(0)), QName.resolveToQName(namespaceService, str))!=null)
-                    map.put(QName.resolveToQName(namespaceService, str) , nodeService.getProperty(new NodeRef(siteNodes.get(0)), QName.resolveToQName(namespaceService, str)));
-                }
-
-                for (QName i : map.keySet()) {
-                        row.createCell(cellNum).setCellValue(createHelper.createRichTextString(i.toPrefixString(namespaceService)));
-                        cellNum++;
-                }
-                rowNum++;
-
-                for (String j : siteNodes) {
-
-                    Map<QName, Serializable> itemsNodes = new HashMap<QName, Serializable>();
-                    for (String str: includeList){
-                        if (nodeService.getProperty(new NodeRef(j), QName.resolveToQName(namespaceService, str))!=null) {
-                            itemsNodes.put(QName.resolveToQName(namespaceService, str), nodeService.getProperty(new NodeRef(j), QName.resolveToQName(namespaceService, str)));
-                        }
-                    }
-
-                    cellNum = 0;
-                    row = sheet.createRow((short) rowNum);
-                    for (QName i : itemsNodes.keySet()) {
-//                        System.out.println(itemsNodes);
-                            row.createCell(cellNum).setCellValue(createHelper.createRichTextString(itemsNodes.get(i).toString()));
-                            cellNum++;
-                    }
-                    rowNum++;
-
-                }
-            }
-            else {
-                Map<QName, Serializable> map = nodeService.getProperties(new NodeRef(siteNodes.get(0)));
-                for (String i: excludeList) {
-                    map.remove(QName.resolveToQName(namespaceService, i));
-                }
-
-                for (QName i : map.keySet()) {
-
-                    row.createCell(cellNum).setCellValue(createHelper.createRichTextString(i.toPrefixString(namespaceService)));
-                    cellNum++;
-                }
-                rowNum++;
-
-                for (String j: siteNodes) {
-//                    System.out.println(j);
-                    Map<QName, Serializable> itemsNodes = nodeService.getProperties(new NodeRef(j));
-                    for (String i: excludeList) {
-                        itemsNodes.remove(QName.resolveToQName(namespaceService, i));
-                    }
-                    cellNum = 0;
-                    row = sheet.createRow((short) rowNum);
-                    for (QName i : itemsNodes.keySet()) {
-                        row.createCell(cellNum).setCellValue(createHelper.createRichTextString(itemsNodes.get(i).toString()));
-                        cellNum++;
-                    }
-                    rowNum++;
-
-                }
-            }
-
-            return wb;
-
-        } catch (Exception e) {
-            //
+    private Row fillRowData(int rowNum, List<String> values, CreationHelper createHelper, Sheet sheet)  {
+        int cellNum = 0;
+        Row row = sheet.createRow((short) rowNum);
+        for (String i : values) {
+            row.createCell(cellNum).setCellValue(createHelper.createRichTextString(i.toString()));
+            cellNum++;
         }
-        return null;
+        return row;
+    }
+
+    private Row fillRowHeader(int rowNum,  List<QName> headers, CreationHelper createHelper, Sheet sheet)  {
+        int cellNum = 0;
+        Row row = sheet.createRow((short) rowNum);
+        for (QName i : headers) {
+            row.createCell(cellNum).setCellValue(createHelper.createRichTextString(i.toPrefixString(namespaceService)));
+            cellNum++;
+        }
+        return row;
     }
 }
